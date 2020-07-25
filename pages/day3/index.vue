ea99eeb9a064879bbf26798b696399b9a1ac1e25
shinobu kambe <kambe4953@gmail.com>
2020-07-25 19:13:51 +0900

    day３を追加

--- pages/day3/index.vue
+++ pages/day3/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container">
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+　<div class="conainer">
+   <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
       <!-- <h1 class="display-4 font-italic">Title of a longer featured blog post</h1> -->
-      <h1 class="display-4 font-italic">シンプルって美しいよね</h1>
+      <h1 class="display-4 font-italic">APIっていいね</h1>
       <!-- <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p> -->
-      <p class="lead my-3">get&postで返ってきたやつ</p>
+      <p class="lead my-3">すごく便利！</p>
       <!-- <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p> -->
       <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">続きを読む</a></p>
     </div>
@@ -27,18 +27,17 @@
           <td>{{item.bady}}</td>
         </tr>
       </tbody>
-    </table>    
+   </table>    
     <nav class="blog-pagination">
         <!-- <a class="btn btn-outline-primary" href="#">Older</a> -->
         <a class="btn btn-outline-primary" href="#">古い記事</a>
         <!-- <a class="btn btn-outline-secondary disabled" href="#">Newer</a> -->
         <a class="btn btn-outline-secondary disabled" href="#">新しい記事</a>
-      </nav>
+    </nav>
   </div>
-  
 </template>
 
-<script>
+ <script>
   import axios from 'axios'
   export default{
    data()　{
@@ -46,7 +45,6 @@
                 message:　'こんにちは',
       　　　　}
    　　　　　},
-
     async asyncData(){
       const url = 'https://jsonplaceholder.typicode.com/posts/'
       const params = {
@@ -61,6 +59,6 @@
       }
     }
  　　　　　　　　 }
-</script>
+ </script>
 
 
