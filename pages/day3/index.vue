747b8eefe0be10c9d525b8e83f2a36c731cd9f72
shinobu kambe <kambe4953@gmail.com>
2020-07-25 16:07:46 +0900

    day3を追加

---
+++ pages/day3/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+   {{message}}
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default{
+  data() {
+    return{
+      message: 'Hellow'
+    }
+  },
+  async asyncData() {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      console.log(response)
+  }
+  }
+</script>
+
+
